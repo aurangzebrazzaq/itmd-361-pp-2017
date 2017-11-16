2ebbf7bd1ffe7cc5799853c10c8e2fbac4c88dcc
aurangzebrazzaq <arazzaq@hawk.iit.edu>
2017-11-15 20:30:06 -0600

    moved event listeners

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,6 +1,19 @@
+$.noConflict();
+
+(function($) {
 
 function ringDoorbell() {
   var doorbell = new Audio('media/doorbell.mp3');
   doorbell.play();
-  document.getElementById("doorbell").addEventListener("click", ringDoorbell);
 }
+
+  function resetdoorBell() {
+    doorbell.currentTime = 0;
+    return;
+  }
+
+//EventListeners
+document.getElementById("doorbell").addEventListener("click", ringDoorbell);
+doorbell.addEventListener("ended", resetdoorBell);
+
+})(jQuery);
