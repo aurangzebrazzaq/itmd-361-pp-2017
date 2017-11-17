0e8a7d5c56f55c65df5da93f3219cba6624e7a59
aurangzebrazzaq <arazzaq@hawk.iit.edu>
2017-11-17 14:45:29 -0600

    added functionality when pressing d on the keyboard

--- pp-05/site.js
+++ pp-05/site.js
@@ -2,18 +2,19 @@ $.noConflict();
 
 (function($) {
 
-function ringDoorbell() {
-  var doorbell = new Audio('media/doorbell.mp3');
-  doorbell.play();
-}
+  function ringDoorbell() {
+    var doorbell = new Audio('media/doorbell.mp3');
+    doorbell.play();
+  }
+  $('html').removeClass('nojs').addClass('js');
 
   function resetdoorBell() {
     doorbell.currentTime = 0;
     return;
   }
 
-//EventListeners
-document.getElementById("doorbell").addEventListener("click", ringDoorbell);
-doorbell.addEventListener("ended", resetdoorBell);
-
+  //EventListeners
+  document.getElementById("doorbell").addEventListener("click", ringDoorbell);
+  window.addEventListener("keydown", ringDoorbell);
+  doorbell.addEventListener("ended", resetdoorBell);
 })(jQuery);
